a66a85f00360288f8d3ceaaa806d66bc0ca86d3a
Vio <herfinaviolap@gmail.com>
2026-04-06 09:05:46 +0700

    semoga db bener

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from src.routes.event import router as event_router
+from src.routes.user_routes import router as user_router
 
 api_router = APIRouter()
 
 api_router.include_router(event_router)
+api_router.include_router(user_router)
