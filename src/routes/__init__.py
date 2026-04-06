acc3e6dcc41a71020f072a6b1ac5ea8f90730c99
Jonnathan Azarels <jonnathanazarelss@Jonnathans-Laptop.local>
2026-04-06 10:55:52 +0700

    feat: menyelesaikan fitur event dan registration bagian jojo

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -5,5 +5,6 @@ from src.routes.role_route import router as role_router
 api_router = APIRouter()
 
 api_router.include_router(event_router)
-
 api_router.include_router(role_router)
+api_router.include_router(event_router)
+api_router.include_router(registration_router)
