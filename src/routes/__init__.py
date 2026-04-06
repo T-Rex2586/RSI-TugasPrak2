7d913c2b616e47e7844f298ac7b8713af8525346
anin <aanindya05@gmail.com>
2026-04-06 16:55:28 +0700

    chore: delete and modify unused files

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter
-from src.routes.event import router as event_router
+from src.routes.account_route import router as account_router
+from src.routes.event_route import router as event_router
+from src.routes.registration_route import router as registration_router
 from src.routes.role_route import router as role_router
 
 api_router = APIRouter()
 
+api_router.include_router(account_router)
 api_router.include_router(event_router)
 api_router.include_router(role_router)
 api_router.include_router(event_router)
